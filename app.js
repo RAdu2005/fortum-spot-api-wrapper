03844dbfadb581387487e20224f72935d73b4a3d
RAdu2005 <radu.stoiana@gmail.com>
2024-09-24 14:15:32 +0300

    features

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.get('/now', async (req, res) => {
 
         res.status(200).json({
             price: priceNow
-        })
+        });
     } catch (error){
         console.error('Error fetching price list', error);
         resizeTo.status(500).json({
@@ -19,13 +19,13 @@ app.get('/now', async (req, res) => {
     }
 });
 
-app.get('/averageToday', async (req, res) => {
+app.get('/average-today', async (req, res) => {
     try{
-        const averageToday = await price.getAverageToday();
+        const average = await price.getAverage("today");
 
         res.status(200).json({
-            price: averageToday
-        })
+            price: average
+        });
     } catch (error){
         console.error('Error fetching price list', error);
         resizeTo.status(500).json({
@@ -33,6 +33,103 @@ app.get('/averageToday', async (req, res) => {
             error: error.message
         })
     }
-})
+});
+
+app.get('/average-tomorrow', async (req, res) => {
+    try{
+        const average = await price.getAverage("tomorrow");
+
+        res.status(200).json({
+            price: average
+        });
+    } catch (error){
+        console.error('Error fetching price list', error);
+        resizeTo.status(500).json({
+            message: 'Error fetching price list',
+            error: error.message
+        })
+    }
+});
+
+app.get('/minimum-today', async (req, res) => {
+    try{
+        const minimum = await price.getMinimum("today");
+
+        res.status(200).json({
+            price: minimum
+        });
+    } catch (error){
+        console.error('Error fetching price list', error);
+        resizeTo.status(500).json({
+            message: 'Error fetching price list',
+            error: error.message
+        })
+    }
+});
+
+app.get('/minimum-tomorrow', async (req, res) => {
+    try{
+        const minimum = await price.getMinimum("tomorrow");
+
+        res.status(200).json({
+            price: minimum
+        });
+    } catch (error){
+        console.error('Error fetching price list', error);
+        resizeTo.status(500).json({
+            message: 'Error fetching price list',
+            error: error.message
+        })
+    }
+});
+
+app.get('/maximum-today', async (req, res) => {
+    try{
+        const maximum = await price.getMaximum("today");
+
+        res.status(200).json({
+            price: maximum
+        });
+    } catch (error){
+        console.error('Error fetching price list', error);
+        resizeTo.status(500).json({
+            message: 'Error fetching price list',
+            error: error.message
+        })
+    }
+});
+
+app.get('/maximum-tomorrow', async (req, res) => {
+    try{
+        const maximum = await price.getMaximum("tomorrow");
+
+        res.status(200).json({
+            price: maximum
+        });
+    } catch (error){
+        console.error('Error fetching price list', error);
+        resizeTo.status(500).json({
+            message: 'Error fetching price list',
+            error: error.message
+        })
+    }
+});
+
+app.get('/next-hour', async (req, res) => {
+    try{
+        const nextHour = await price.getNextHour();
+
+        res.status(200).json({
+            price: nextHour
+        });
+    } catch (error){
+        console.error('Error fetching price list', error);
+        resizeTo.status(500).json({
+            message: 'Error fetching price list',
+            error: error.message
+        })
+    }
+});
+
 
 module.exports = app;
--- processor.js
+++ processor.js
@@ -1,7 +1,24 @@
 const price = require('./cacher');
 const utils = require('./utils');
 
-async function getNow(){
+function dayToNumber(day){
+    if (day == "today") {return 0;}
+    return 1;
+}
+
+function checkTomorrow(day){
+    if (day == 0) {return true;}
+    
+    const now = new Date();
+    if (now.getHours() >= 15){
+        console.log("it's past 15");
+        return true;
+    }
+    return false;
+}
+
+module.exports.getNow = async (day) => {
+    day = dayToNumber(day);
     const priceList = await price.cache();
 
     let now = new Date();
@@ -19,15 +36,66 @@ async function getNow(){
     return null;
 }
 
-async function getAverageToday(){
+module.exports.getAverage = async (day) => {
+    day = dayToNumber(day);
+    if(!checkTomorrow(day)) {return -1;}
+
     const priceList = await price.cache();
 
     let sumOfPrices = 0;
-    for (let i in priceList[0].series) {
-        sumOfPrices += priceList[0].series[i].value;
+    for (let i in priceList[day].series) {
+        sumOfPrices += priceList[day].series[i].value;
     }
 
     return Math.round(sumOfPrices / 24 * 100) / 100;
 }
 
-module.exports = {getNow, getAverageToday};
+module.exports.getMaximum = async (day) => {
+    day = dayToNumber(day);
+    if(!checkTomorrow(day)) {return -1;}
+
+    const priceList = await price.cache();
+
+    let max = -1;
+    for(let i in priceList[day].series) {
+        if (priceList[day].series[i].value > max){
+            max = priceList[day].series[i].value;
+        }
+    }
+
+    return Math.round(max * 100) / 100;
+}
+
+module.exports.getMinimum = async (day) => {
+    day = dayToNumber(day);
+    if(!checkTomorrow(day)) {return -1;}
+
+    const priceList = await price.cache();
+
+    let min = priceList[day].series[0].value;
+    for(let i in priceList[day].series) {
+        if(i == 0) {continue;}
+        if (priceList[day].series[i].value < min){
+            min = priceList[day].series[i].value;
+        }
+    }
+
+    return Math.round(min * 100) / 100;
+}
+
+module.exports.getNextHour = async () => {
+    const priceList = await price.cache();
+
+    const now = new Date();
+    if(now.getHours() == 23){
+        return Math.round(priceList[1].series[0].value * 100) / 100;
+    } else {
+        now.setHours(now.getHours() + 1, 0, 0, 0);
+        const nowString = utils.toLocalISOString(now).slice(0, -11);
+        for (let i in priceList[0].series){
+            if (priceList[0].series[i].startDate.slice(0, -3) == nowString){
+                return Math.round(priceList[0].series[i].value * 100) / 100;
+            }
+        }
+    }
+}
